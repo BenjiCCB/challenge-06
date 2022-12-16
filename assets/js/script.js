76ec4832b07e1dabd93447e14b1c258446e50599
BenjiCCB <bensdecker+CCB@gmail.com>
2022-12-16 12:05:58 -0500

    follow up commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,23 +29,41 @@ var buttonClickHandler = function (event) {
   }
 };
 
-var getForecastData = function (city) {
-  var apiUrl = 'https://api.github.com/users/' + city + '/repos';
+function getForecastData(searchCity){
+  var testUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=78e5804b571e08c79ef4568f6738f1c2"
 
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayForecast(data, city);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to OpenWeather');
-    });
+  fetch(testUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        // for(var i = 0; i < 5 ; i++){
+        //   console.log(data.list[i*8].dt_txt +": " + data.list[i*8].main.temp)  
+        // }
+        displayForecast(data, searchCity);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  });
 };
+
+// var getForecastData = function (city) {
+//   var apiUrl = 'https://api.github.com/users/' + city + '/repos';
+
+//   fetch(apiUrl)
+//     .then(function (response) {
+//       if (response.ok) {
+//         response.json().then(function (data) {
+//           displayForecast(data, city);
+//         });
+//       } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert('Unable to connect to OpenWeather');
+//     });
+// };
 
 // var getFeaturedRepos = function (language) {
 //   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
@@ -61,34 +79,36 @@ var getForecastData = function (city) {
 //   });
 // };
 
-var displayForecast = function (forecastDays, searchTerm) {
-  if (forecastDays.length === 0) {
-    forecastContainerEl.textContent = 'No repositories found.';
+var displayForecast = function (forecastInfo, searchTerm) {
+  if (forecastInfo.length === 0) {
+    forecastContainerEl.textContent = 'No forecast data found.';
     return;
   }
 
+  // console.log(forecastInfo.list[0].main.temp);
+
   citySearchTerm.textContent = searchTerm;
 
-  for (var i = 0; i < forecastDays.length; i++) {
-    var cityName = forecastDays[i].owner.login + '/' + forecastDays[i].name;  // ****CHANGE****
+  for (var i = 0; i < 5; i++) {
+    var forecastItem = forecastInfo.city.name + " - " + forecastInfo.list[i*8].dt_txt + "... " + forecastInfo.list[i*8].main.temp;
 
     var focecastEl = document.createElement('div');
     focecastEl.classList = 'list-item flex-row justify-space-between align-center';
 
     var titleEl = document.createElement('span');
-    titleEl.textContent = cityName;
+    titleEl.textContent = forecastItem;
 
     focecastEl.appendChild(titleEl);
 
     var statusEl = document.createElement('span');
     statusEl.classList = 'flex-row align-center';
 
-    if (forecastDays[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + forecastDays[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
+    // if (forecastInfo[i].open_issues_count > 0) {
+    //   statusEl.innerHTML =
+    //     "<i class='fas fa-times status-icon icon-danger'></i>" + forecastDays[i].open_issues_count + ' issue(s)';
+    // } else {
+    //   statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    // }
 
     focecastEl.appendChild(statusEl);
 
@@ -97,20 +117,7 @@ var displayForecast = function (forecastDays, searchTerm) {
 };
 
 
-function showTestData(){
-  var testUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=london&appid=78e5804b571e08c79ef4568f6738f1c2'
 
-  fetch(testUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        console.log(data.list[0].main.temp)
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
 
 // var testUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=40.7128&lon=74.0060&appid=78e5804b571e08c79ef4568f6738f1c2' -- lat/long
 // var testUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=40.7128&lon=74.0060&cnt=5&appid=78e5804b571e08c79ef4568f6738f1c2' -- lat/long daily (doesn't work)
@@ -134,7 +141,7 @@ function showTestData(){
 // };
 
 
-showTestData();
+getForecastData("New york");
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
 cityButtonsEl.addEventListener('click', buttonClickHandler);

0c276ce45f08bbfade5645246e166e601994fdc1
BenjiCCB <bensdecker+CCB@gmail.com>
2022-12-16 16:45:58 -0500

    dayjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,20 @@ var cityButtonsEl = document.querySelector('#city-buttons');
 var cityInputEl = document.querySelector('#cityname');
 var forecastContainerEl = document.querySelector('#forecasts-container');
 var citySearchTerm = document.querySelector('#city-search-term');
+var subtitleText = document.querySelector('.subtitle');
 
+
+// Form
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
   var searchCity = cityInputEl.value.trim();
 
   if (searchCity) {
-    displayHistoryButtons(searchCity);
+    updateHistoryButtons(searchCity);
     getForecastData(searchCity);
 
+    // subtitleText.style.display = 'inline'
     forecastContainerEl.textContent = '';
     cityInputEl.value = '';
 
@@ -21,26 +25,32 @@ var formSubmitHandler = function (event) {
   }
 };
 
-
-function displayHistoryButtons(queryCity){
-  
-  
+// Buttons
+function displayHistoryButtons(){
   var searchCitiesArray = JSON.parse(localStorage.getItem("searchCities")) || [];
-  searchCitiesArray.push(queryCity);
-  localStorage.setItem("searchCities", JSON.stringify(searchCitiesArray));
+  var searchCitiesArraySorted = searchCitiesArray.reverse();
+  cityButtonsEl.textContent = "";
 
-
-  for(var i = 0; i < 10 && i < searchCitiesArray.length; i++){
+  for(var i = 0; i < 10 && i < searchCitiesArraySorted.length; i++){
     var cityButtonEL = document.createElement("button");
     cityButtonEL.setAttribute("class", "btn");
-    cityButtonEL.setAttribute("data-city", queryCity)
-    cityButtonEL.innerHTML = queryCity;
+    cityButtonEL.setAttribute("data-city", searchCitiesArraySorted[i]);
+    cityButtonEL.innerHTML = searchCitiesArraySorted[i];
     cityButtonsEl.appendChild(cityButtonEL);  
   }
 }
 
+function updateHistoryButtons(queryCity){
+  var searchCitiesArray = JSON.parse(localStorage.getItem("searchCities")) || [];
+  searchCitiesArray.push(queryCity);
+  localStorage.setItem("searchCities", JSON.stringify(searchCitiesArray));
+
+  displayHistoryButtons();
+}
+
 var buttonClickHandler = function (event) {
   var buttonCity = event.target.getAttribute('data-city');
+  // subtitleText.style.display = 'inline'
 
   if (buttonCity) {
     getForecastData(buttonCity);
@@ -48,6 +58,7 @@ var buttonClickHandler = function (event) {
   }
 };
 
+// Forecast Data
 function getForecastData(searchCity){
   var testUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity +"&units=imperial&appid=78e5804b571e08c79ef4568f6738f1c2"
 
@@ -63,37 +74,6 @@ function getForecastData(searchCity){
   });
 };
 
-// var getForecastData = function (city) {
-//   var apiUrl = 'https://api.github.com/users/' + city + '/repos';
-
-//   fetch(apiUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           displayForecast(data, city);
-//         });
-//       } else {
-//         alert('Error: ' + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert('Unable to connect to OpenWeather');
-//     });
-// };
-
-// var getFeaturedRepos = function (language) {
-//   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-//   fetch(apiUrl).then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         displayRepos(data.items, language);
-//       });
-//     } else {
-//       alert('Error: ' + response.statusText);
-//     }
-//   });
-// };
 
 var displayForecast = function (forecastInfo, searchTerm) {
   if (forecastInfo.length === 0) {
@@ -105,6 +85,8 @@ var displayForecast = function (forecastInfo, searchTerm) {
 
   for (var i = 0; i < 5; i++) {
     var forecastItem = forecastInfo.city.name + " - " + forecastInfo.list[i*8].dt_txt + "... " + forecastInfo.list[i*8].main.temp;
+
+    console.log("1671256800".format('MMM D'));
 
     var focecastEl = document.createElement('div');
     focecastEl.classList = 'list-item flex-row justify-space-between align-center';
@@ -131,31 +113,6 @@ var displayForecast = function (forecastInfo, searchTerm) {
 };
 
 
-
-
-// var testUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=40.7128&lon=74.0060&appid=78e5804b571e08c79ef4568f6738f1c2' -- lat/long
-// var testUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily?lat=40.7128&lon=74.0060&cnt=5&appid=78e5804b571e08c79ef4568f6738f1c2' -- lat/long daily (doesn't work)
-// var testUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=london&appid=78e5804b571e08c79ef4568f6738f1c2' - city search
-// var testUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=78e5804b571e08c79ef4568f6738f1c2' - city info (for geocoding)
-
-
-// function showTestData(){
-//   var testUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=78e5804b571e08c79ef4568f6738f1c2'
-
-//   fetch(testUrl).then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         console.log(data[0].lat);
-//         console.log(data[0].lon);
-//       });
-//     } else {
-//       alert('Error: ' + response.statusText);
-//     }
-//   });
-// };
-
-
-// getForecastData("New york");
-
+displayHistoryButtons();
 cityFormEl.addEventListener('submit', formSubmitHandler);
 cityButtonsEl.addEventListener('click', buttonClickHandler);
